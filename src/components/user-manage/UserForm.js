e720ee7905459d5516b842688c1f241ccc0af5a1
supermeng <76035703+weimeng0910@users.noreply.github.com>
2022-05-09 11:32:13 +0300

    09052022 commit

--- src/components/user-manage/UserForm.js
+++ src/components/user-manage/UserForm.js
@@ -1,89 +1,129 @@
-import React, { useRef, forwardRef, useImperativeHandle } from 'react';
+import React, {
+    useState,
+    useRef,
+    forwardRef,
+    useImperativeHandle,
+} from 'react';
 //导入antd组件
 import { Form, Input, Select } from 'antd';
 
 const { Option } = Select;
 
+// forward用于获取函数式组件DOM元素
 const UserForm = forwardRef((props, ref) => {
+    // console.log(ref);
+    //使用Ref创建子组件的引用变量
     const formRef = useRef(null);
+    //const [form] = Form.useForm();
+    //设置禁用选项的状态
+    const [isDisabled, setIsDisabled] = useState(false);
+
+    //ImperativeHandle，他的作用是“勾住”子组件中的某个函数（方法）供父组件调用。
     // 作用: 减少父组件获取的DOM元素属性,只暴露给父组件需要用到的DOM方法
     // 参数1: 父组件传递的ref属性
     // 参数2: 返回一个对象,父组件通过ref.current调用对象中方法
+    //第 3 个参数是函数的依赖。如果处理函数中的任何依赖项，可以设置没有任何依赖项的参数
+    //通过使用ImperativeHandle函数，将函数封装成一个对象，并将该对象定义到父组件内部的ref中。
 
     useImperativeHandle(
         ref,
+
         () => ({
+            //子组件定义内部函数 validateFields充许父组件访问这个方法，开放给父组件
+            //通过useImperativeHandle的Hook, 将父组件传入的ref和useImperativeHandle第二个参数返回的对象绑定到了一起
             validateFields: () => formRef.current.validateFields(),
-        }),
-        [formRef]
+            resetFields: () => formRef.current.resetFields(),
+            setFieldsValue,
+            setIsDisabled,
+        })
     );
-    // console.log(props, ref);
+    //重新封装setFieldsValue方法，开放给父组件
+    const setFieldsValue = (v) => {
+        formRef.current.setFieldsValue(v);
+    };
+    //将父组件传过来的数据从props中解构
     const { roleList, regionList } = props;
     return (
         <Form ref={formRef} layout='vertical'>
-            <Form.Item label='用户名'>
-                <Form.Item
-                    name='username'
-                    // 校验
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input the title of collection!',
-                        },
-                    ]}>
-                    <Input />
-                </Form.Item>
+            <Form.Item
+                name='username'
+                label='用户名'
+                // 校验
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input the title of collection!',
+                    },
+                ]}>
+                <Input />
             </Form.Item>
-            <Form.Item label='密码'>
-                <Form.Item
-                    name='password'
-                    // 校验
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input the title of collection!',
-                        },
-                    ]}>
-                    <Input />
-                </Form.Item>
+
+            <Form.Item
+                name='password'
+                label='密码'
+                // 校验
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input the title of collection!',
+                    },
+                ]}>
+                <Input />
             </Form.Item>
-            <Form.Item label='区域'>
-                <Form.Item
-                    name='region'
-                    // 校验
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input the title of collection!',
-                        },
-                    ]}>
-                    <Select style={{ width: 120 }}>
-                        {regionList.map((item) => (
-                            <Option value={item.value} key={item.id}>
-                                {item.title}
-                            </Option>
-                        ))}
-                    </Select>
-                </Form.Item>
+
+            <Form.Item
+                name='region'
+                label='区域'
+                // 校验
+                rules={
+                    isDisabled
+                        ? []
+                        : [
+                              {
+                                  required: true,
+                                  message:
+                                      'Please input the title of collection!',
+                              },
+                          ]
+                }>
+                <Select disabled={isDisabled} style={{ width: 120 }}>
+                    {regionList.map((item) => (
+                        <Option value={item.value} key={item.id}>
+                            {item.title}
+                        </Option>
+                    ))}
+                </Select>
             </Form.Item>
-            <Form.Item label='角色'>
-                <Form.Item
-                    name='roleId'
-                    // 校验
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input the title of collection!',
-                        },
-                    ]}>
-                    <Select style={{ width: 120 }}>
-                        {roleList.map((item) => (
-                            <Option value={item.id} key={item.id}>
-                                {item.roleName}
-                            </Option>
-                        ))}
-                    </Select>
-                </Form.Item>
+
+            <Form.Item
+                name='roleId'
+                label='角色'
+                // 校验
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input the title of collection!',
+                    },
+                ]}>
+                <Select
+                    onChange={(value) => {
+                        if (value === 1) {
+                            setIsDisabled(true);
+                            //antd中setFieldsValue设置当前的form中正在输入的input值
+                            formRef.current.setFieldsValue({
+                                region: '全球 ',
+                            });
+                        } else {
+                            setIsDisabled(false);
+                        }
+                    }}
+                    style={{ width: 120 }}>
+                    {roleList.map((item) => (
+                        <Option value={item.id} key={item.id}>
+                            {item.roleName}
+                        </Option>
+                    ))}
+                </Select>
             </Form.Item>
         </Form>
     );
--- src/views/sandbox/user-manage/UserList.js
+++ src/views/sandbox/user-manage/UserList.js
@@ -5,7 +5,7 @@ import { Table, Button, Modal, Switch } from 'antd';
 //导入ico
 import {
     DeleteOutlined,
-    UnorderedListOutlined,
+    EditOutlined,
     ExclamationCircleOutlined,
 } from '@ant-design/icons';
 //导入子组件Form表单
@@ -14,19 +14,25 @@ import UserForm from '../../../components/user-manage/UserForm';
 const { confirm } = Modal;
 
 export default function UserList() {
-    //穿透子组件获取子组件的表单属性
-    const formRef = useRef(null);
+    //定义父组件的Ref传递给子组件，穿透子组件获取子组件的表单属性
+    const userListRef = useRef(null);
+    //const updateForm = useRef(null);
     //数据状态
     const [dataSource, setDataSource] = useState([]);
     //显示弹出表单的状态
     const [isAddVisible, setIsAddVisible] = useState(false);
+    //弹出修改的状态
+    const [isUpdateVisible, setIsUpdateVisible] = useState(false);
     const [roleList, setRoleList] = useState([]);
     const [regionList, setRegionList] = useState([]);
+    // const [isUpdateDisabled, setisUpdateDisabled] = useState(false);
+
     //获取数据
     useEffect(() => {
         axios.get('http://localhost:5000/users?_expand=role').then((res) => {
             //console.log(res.data);
-            setDataSource(res.data);
+            const list = res.data;
+            setDataSource(list);
         });
     }, []);
     //表单中读取的区域的数据获得
@@ -43,13 +49,14 @@ export default function UserList() {
             setRoleList(res.data);
         });
     }, []);
+
     //定义表格结构数据
     const columns = [
         {
             title: '区域',
             dataIndex: 'region',
             render: (region) => {
-                return <b>{region === '' ? '全球' : region}</b>;
+                return <b>{region != '' ? region : '全球'}</b>;
             },
         },
         {
@@ -70,37 +77,69 @@ export default function UserList() {
                 return (
                     <Switch
                         checked={roleState}
-                        disabled={item.default}></Switch>
+                        disabled={item.default}
+                        onChange={() => handleChange(item)}></Switch>
                 );
             },
         },
         {
             title: '操作',
-
             render: (item) => {
                 return (
                     <div>
-                        <span style={{ margin: '0 10px' }}>
-                            <Button
-                                danger
-                                shape='circle'
-                                onClick={() => removeConfirm(item)}
-                                icon={<DeleteOutlined />}
-                                disabled={item.default}></Button>
-                        </span>
+                        <Button
+                            danger
+                            shape='circle'
+                            icon={<DeleteOutlined />}
+                            onClick={() => confirmMethod(item)}
+                            disabled={item.default}
+                        />
 
                         <Button
                             type='primary'
                             shape='circle'
-                            icon={<UnorderedListOutlined />}
-                            disabled={item.default}></Button>
+                            icon={<EditOutlined />}
+                            disabled={item.default}
+                            onClick={() => handleUpdate(item)}
+                        />
                     </div>
                 );
             },
         },
     ];
+    //弹出更新模态框
+    const handleUpdate = (item) => {
+        //对话框模块还没显示，也就是表单还没挂载就调用了setFieldsValue，所以报错添加setTimeout
+        //handleUpdate Antd 表单是异步的,所以要添加setTimeout
+        setTimeout(() => {
+            setIsUpdateVisible(true);
+            if (item.roleId === 1) {
+                //禁用
+                userListRef.current.setIsDisabled(true);
+            } else {
+                //取消禁用
+                userListRef.current.setIsDisabled(false);
+            }
+
+            //console.log(userListRef.current.form.setFieldsValue(item));
+            userListRef.current.setFieldsValue(item);
+        }, 0);
+    };
+    //更新提交
+    const updateFormOK = () => {
+        console.log('111');
+    };
+    //关闭角色的按钮处理函数
+    const handleChange = (item) => {
+        item.roleState = !item.roleState;
+        setDataSource([...dataSource]);
+        //更新数据
+        axios.patch(`http://localhost:5000/users/${item.id}`, {
+            roleState: item.roleState,
+        });
+    };
     //删除处理函数
-    const removeConfirm = (item) => {
+    const confirmMethod = (item) => {
         confirm({
             title: '你确定要删除?',
             icon: <ExclamationCircleOutlined />,
@@ -116,44 +155,52 @@ export default function UserList() {
     };
     //删除
     const deleteMethod = (item) => {
-        console.log(item);
+        //console.log(item);
         // 当前页面同步状态 + 后端同步
-        // if (item.grade === 1) {
-        //     setDataSource(dataSource.filter((data) => data.id !== item.id));
-        //     axios.delete(`http://localhost:5000/rights/${item.id}`);
-        // } else {
-        //     let list = dataSource.filter((data) => data.id === item.rightId);
-        //     list[0].children = list[0].children.filter(
-        //         (data) => data.id !== item.id
-        //     );
-        //     setDataSource([...dataSource]);
-        //     axios.delete(`http://localhost:5000/children/${item.id}`);
-        // }
+        setDataSource(dataSource.filter((data) => data.id !== item.id));
+        axios.delete(`http://localhost:5000/users/${item.id}`);
     };
+    //确定提交
     const addFormOK = () => {
-        formRef.current
+        //点击确定后，访问子组件开放给父组件的方法，从而获得form表单的value
+        //在父组件中, 调用Ref.current时, 实际上是useImperativeHandle第二个参数返回的对象
+        userListRef.current
             .validateFields()
             .then((value) => {
-                console.log(value);
-
-                // setisAddVisible(false);
+                //console.log(value);
+                //关闭模态框
+                setIsAddVisible(false);
+                //重置表单
+                //console.log(userListRef.current);
+                userListRef.current.resetFields();
 
                 //post到后端，生成id，再设置 datasource, 方便后面的删除和更新
-                // axios
-                //     .post(`http://localhost:5000/users`, {
-                //         ...value,
-                //         roleState: true,
-                //         default: false,
-                //     })
-                //     .then((res) => {
-                //         console.log(res.data);
-                //         setdataSource([...dataSource, res.data]);
-                //     });
+                axios
+                    .post(`http://localhost:5000/users`, {
+                        //组装数据
+                        ...value,
+                        roleState: true,
+                        default: false,
+                    })
+                    .then((res) => {
+                        console.log(res.data);
+                        setDataSource([
+                            ...dataSource,
+                            {
+                                ...res.data,
+
+                                role: roleList.filter(
+                                    (item) => item.id === value.roleId
+                                )[0],
+                            },
+                        ]);
+                    });
             })
             .catch((err) => {
                 console.log(err);
             });
     };
+
     return (
         <>
             <Button
@@ -164,16 +211,14 @@ export default function UserList() {
                 添加用户
             </Button>
             <Table
-                dataSource={dataSource}
                 columns={columns}
+                dataSource={dataSource}
                 //分页
                 pagination={{
                     pageSize: 5,
                 }}
                 //给遍历的每一项分配key值唯一
                 rowKey={(item) => item.id}
-                //loading
-                loading={false}
             />
             {/* 弹出表单 */}
             <Modal
@@ -183,12 +228,30 @@ export default function UserList() {
                 cancelText='取消'
                 onCancel={() => {
                     setIsAddVisible(false);
+                    userListRef.current.resetFields();
                 }}
                 onOk={() => addFormOK()}>
                 <UserForm
                     roleList={roleList}
                     regionList={regionList}
-                    ref={formRef}
+                    ref={userListRef}
+                />
+            </Modal>
+            {/* 修改选中项弹出的模态框 */}
+            <Modal
+                visible={isUpdateVisible}
+                title='修改用户'
+                okText='更新'
+                cancelText='取消'
+                forceRender={true}
+                onCancel={() => {
+                    setIsUpdateVisible(false);
+                }}
+                onOk={() => updateFormOK()}>
+                <UserForm
+                    roleList={roleList}
+                    regionList={regionList}
+                    ref={userListRef}
                 />
             </Modal>
         </>
